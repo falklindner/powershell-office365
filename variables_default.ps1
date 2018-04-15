30709912ca762e4b5d99de8bb587a950754b6c86
Falk <lindner.falk@googlemail.com>
2018-04-15 12:32:37 +0200

    Generated variables_default for static variables

---
+++ variables_default.ps1
@@ -0,0 +1,8 @@
+﻿$ExcelPath = "$(Get-Item $PSScriptRoot)\work"
+$LOG = "$ExcelPath\Log\Log.txt"
+$TestCMDs = "$ExcelPath\Log\CMDs.txt"
+
+$User = "Username"
+$PW =  ConvertTo-SecureString -String "Password" -AsPlainText -Force
+$UserCredential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $User, $PW 
+$SPOnline=path-to-sharepoint.com
